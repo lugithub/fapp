8de6d6c6a31558f4a625b865620335ba9bbee9fb
Bin Lu <lubinlubin@gmail.com>
2020-11-16 23:25:04 -0500

    Functors allow us to compose an effectful program, e.g. g, with a pure program, e.g. add(10). But the pure program must be unary.

--- src/a-functor.ts
+++ src/a-functor.ts
@@ -1,6 +1,17 @@
-import { map, some, none } from 'fp-ts/Option';
+import { flow } from 'fp-ts/function';
+import { map, some, none, Option } from 'fp-ts/Option';
+import { length, add, prop, path } from 'rambda';
 
 export function f() {
-  //   return map<number, number>(x => x + 1)(some(1));
-  return map<number, number>(x => x + 1)(none);
+  // return map<number, number>(x => x + 1)(some(1));
+  // return map<number, number>(x => x + 1)(none);
+
+  // map(x=>x+1) . g
+  // where x => x + 1, a pure function
+  //       g,          an effectual function
+  return flow(g, map<number, number>(add(10)))(some('hi'));
+}
+
+function g(a: Option<string>) {
+  return map<string, number>(prop('length'))(a);
 }
