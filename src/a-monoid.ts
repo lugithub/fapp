db9ea630c161b420a7ab0dadf2530ae0bbdbf0ca
Bin Lu <lubinlubin@gmail.com>
2020-11-25 21:46:11 -0500

    any

--- src/a-monoid.ts
+++ src/a-monoid.ts
@@ -13,6 +13,9 @@ import {
   monoidString,
   monoidSum,
 } from 'fp-ts/Monoid';
+import { all, any, equals } from 'rambda';
+import { map } from 'fp-ts/lib/Array';
+import { flow } from 'fp-ts/lib/function';
 
 export function m() {
   //   return fold(monoidSum)([1, 2, 3, 4]); // 10
@@ -25,5 +28,23 @@ export function m() {
   //   return fold(getApplyMonoid(monoidSum))([some(1), some(10)]);
 
   //   return fold(getFirstMonoid<number>())([none, none, some(1), some(2)]);
-  return fold(getLastMonoid<number>())([some(1), some(2), none, none]);
+  // return fold(getLastMonoid<number>())([some(1), some(2), none, none]);
+
+  // const list = [1, 2, 3, 4, 5];
+  // const predicate = (x: number) => x * x > 8;
+  // return all(predicate, list);
+
+  type Status = 1 | 2 | 3 | 4 | 5;
+  // const validList = [3, 5];
+  const a = 5;
+
+  const validFoo = (a: Status) => a === 3 || a === 5;
+  const validFoo3 = (a: Status) => fold(monoidAny)([a === 3, a === 5]);
+  return validFoo3;
+
+  // const validFoo2 = flow(
+  //   map((f: (b: number) => boolean) => f(a)),
+  //   fold(monoidAny)
+  // )([equals(3), equals(5)]);
+  // return validFoo2;
 }
